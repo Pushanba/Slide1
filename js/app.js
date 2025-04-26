b1a6adb0dd3761568f5f3593c021774cad49afaf
Pushan <pushanbarua46@gmail.com>
2025-04-26 17:22:42 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){ 
+
+
+    $(document).ready(function(){
+
+
+        $('.benarSlider').slick({
+          prevArrow: "#benarLeftErow",
+          nextArrow: "#benarRightErow",
+            dots: true,
+            dotsClass: 'slick-dots',
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+
+
+            
+            
+       });
+      });
+
+
+
+      
+
+       
+        
+
+
+
+
+
+
+
+}) 
